04293e4105ac8b7cba7905dc8ac5dbb058e614f1
MehdiSassi92 <mehdi.sassi.180192@gmail.com>
2022-07-15 00:47:08 +0100

    adding express checkpoint

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+
+const {logger} = require('./middleware/logger');
+
+const app = express();
+
+app.use(logger)
+
+app.use(express.json())
+
+app.use(express.static(__dirname +"/public"))
+
+const PORT = 4000;
+app.listen(PORT, (err)=>{
+    err
+    ?console.log(err) 
+    :console.log(`The server is running in port ${PORT}...`);
+})
